5728a799d6e6a944369011abab39b5c0b52ab36d
Thomas Vivien <thomas.yang.vivien@ik.me>
2023-08-13 21:03:51 +0200

    feat: create url:link command

---
+++ app/Commands/UrlLinkCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Commands;
+
+use App\Exceptions\AppException;
+use App\Services\Builders\DownloadBuilder;
+use App\Services\Driver\DriverService;
+use App\Services\File\Download;
+use App\Services\File\Renamer;
+use App\Services\Output\ColoredStringWriter;
+
+class UrlLinkCommand extends AbstractCommand
+{
+    protected $signature = 'url:link
+        {urls* : the urls to download from any host}';
+
+    protected $description = 'Get the premium links of a hosted file';
+
+    protected DriverService $driverService;
+
+    public function __construct(DriverService $driverService)
+    {
+        parent::__construct();
+
+        $this->driverService = $driverService;
+    }
+
+    protected function _handle()
+    {
+        $urls = [];
+        $hasError = false;
+
+        $downloadBuilder = new DownloadBuilder();
+        $downloadBuilder->setDriverService($this->driverService);
+        $downloadBuilder->setOutput($this->output);
+
+        $input = explode(' ', $this->argument('urls')[0]);
+
+        foreach ($input as $index => $url) {
+            try {
+                $download = $downloadBuilder->build($url);
+                $urls[] = $download->getUrl();
+            } catch (AppException $exception) {
+                $hasError = true;
+                $this->line("Error while processing $url:");
+                $this->line((new ColoredStringWriter())->getColoredString($exception->getMessage(), 'red'));
+            }
+        }
+
+        if (empty($urls)) {
+            return;
+        }
+
+        if ($hasError) {
+            $this->line(
+                (new ColoredStringWriter())
+                    ->getColoredString('Url that have generated errors are not listed below', 'red'),
+            );
+        }
+
+        foreach ($urls as $url) {
+            $this->line($url);
+        }
+    }
+}
